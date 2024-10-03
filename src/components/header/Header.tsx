014f4f96cb57b68a7840428d24b919b80691ed80
avinashbatchala <hariprasadnarla4@gmail.com>
2024-10-03 21:36:09 +0530

    updated styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../assets/logo.svg';
 
 const Header: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
       <header className="header">
         <Link to="/">
           <img src={logo} alt="Dream Manali Tours Logo" className="logo" />
         </Link>
-        <button className="menu-button" onClick={toggleMenu}>
-          <div className={`hamburger ${isMenuOpen ? 'open' : ''}`}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </button>
-        <nav className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
+        <nav className="nav">
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><a href="#services">Services</a></li>
