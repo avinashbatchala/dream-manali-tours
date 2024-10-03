45c57d36171b510192572b8612fdab9fcf25324f
avinashbatchala <hariprasadnarla4@gmail.com>
2024-10-04 02:01:25 +0530

    updated styling to fluent ui

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../assets/logo.svg';
 
 const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
       <header className="header">
         <Link to="/">
           <img src={logo} alt="Dream Manali Tours Logo" className="logo" />
         </Link>
-        <nav className="nav">
+        <button className="menu-button" onClick={toggleMenu}>
+          &#9776;
+        </button>
+        <nav className={`nav ${menuOpen ? 'open' : ''}`}>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+            <li><a href="#services" onClick={toggleMenu}>Services</a></li>
+            <li><a href="#about" onClick={toggleMenu}>About</a></li>
+            <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
           </ul>
         </nav>
       </header>
